7e646b23ff010192271a1fa54a46c99696898508
shmuel keller <shmuli@Mac.attlocal.net>
2024-12-17 04:10:53 -0500

    ...

--- task-calendar/vite.config.ts
+++ task-calendar/vite.config.ts
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  base: '/repo-name/', // Replace 'repo-name' with your GitHub repository name
+  base: '/trello/', // Replace 'repo-name' with your GitHub repository name
   server: {
     port: 3000, // Development server runs on port 3000
   },
